2427a829f24a3f491b7b841bc7ecf677993a8653
MauriceVayuz <maurice.vayuz@gmail.com>
2023-05-24 17:15:27 +0530

    shippingpolicy

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,6 +7,14 @@ export default function About() {
                 As part of our innovative SAAS, we introduced data driven POS(Point of Sale)which includes generative AI CRM and platform based Loyalty program & a third party delivery platform which works on dynamic price discovery model based on market supply and demand.
                 Both the Components of our SaaS facilitates the trifecta model which includes engagement, Retention and Transparency making the restaurants life much easier!
             </p>
+            <h1 className="text-4xl font-semibold" style={{marginTop:"60px"}}>Shipping Policy</h1>
+            <h3 style={{fontSize:"24px", marginTop:"20px", fontWeight:"600"}}>Order Processing and Shipping Time</h3>
+            <p className="w-11/12 mt-16 text-base lg:w-10/12 lg:ml-16 md:mr-16">
+            Street food vendors takes 10-20mins depending upon the demand A delivery guy 
+            takes 10-15mins to deliver depending upon the distance he have to deliver Shipping Cost
+            Depends on Number of kilometre. For first 3 km it is fixed i.e 20rs. After every 
+            kilometre cost is increased International Shipping N.A
+            </p>
         </div>
     )
 }
